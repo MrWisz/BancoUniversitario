2392d7eef080a99883189bcf4fd773a0582cef40
MrWisz <eduardoluis09@hotmail.com>
2024-11-22 13:35:07 -0400

    Pagina de login 100% responsive
    iconos de redes sociales creados y redireccionados a redes falsas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,24 +4,42 @@ import LoginForm from "../components/LoginForm";
 import HeaderBanca from '../components/HeaderBanca';
 import Footer from '../components/Footer';
 
-
 const FrameParent = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-self: stretch;
-  align-items: center; /* Cambiado a center */
+  align-items: center;
   gap: 71px;
   max-width: 100%;
   text-align: center;
   font-size: var(--font-size-5xl);
   color: var(--color);
   font-family: var(--font-montserrat-alternates);
+
   @media screen and (max-width: 1300px) {
     gap: 35px;
+    font-size: var(--font-size-4xl);
   }
+
   @media screen and (max-width: 900px) {
     gap: var(--gap-lg);
+    font-size: var(--font-size-3xl);
+  }
+
+  @media screen and (max-width: 600px) {
+    gap: var(--gap-md);
+    font-size: var(--font-size-2xl);
+  }
+
+  @media screen and (max-width: 400px) {
+    gap: var(--gap-sm);
+    font-size: var(--font-size-xl);
+  }
+
+  @media screen and (max-width: 290px) {
+    gap: var(--gap-xs);
+    font-size: var(--font-size-lg);
   }
 `;
 
@@ -37,26 +55,39 @@ const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+
   @media screen and (max-width: 1300px) {
     gap: 37px;
   }
+
   @media screen and (max-width: 900px) {
     gap: var(--gap-lgi);
+  }
+
+  @media screen and (max-width: 600px) {
+    gap: var(--gap-md);
+  }
+
+  @media screen and (max-width: 400px) {
+    gap: var(--gap-sm);
+  }
+
+  @media screen and (max-width: 290px) {
+    gap: var(--gap-xs);
   }
 `;
 
 const Login = () => {
-    return (
-        <LoginContainer>
-            <FrameParent>
-                <HeaderBanca />
-                <LoginForm />
-                <FrameComponent4 />
-            </FrameParent>
-            <Footer/>
-        </LoginContainer>
-        
-    );
+  return (
+    <LoginContainer>
+      <FrameParent>
+        <HeaderBanca />
+        <LoginForm />
+        <FrameComponent4 />
+      </FrameParent>
+      <Footer />
+    </LoginContainer>
+  );
 };
 
 export default Login;
