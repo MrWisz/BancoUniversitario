866ef18502025efef12449296c8deeaf7ac6a19f
MrWisz <eduardoluis09@hotmail.com>
2024-12-07 00:06:35 -0400

    8080

--- src/services/axiosApi.js
+++ src/services/axiosApi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { getJWT } from '../utils/localStorage';
 
 //export const API_URL_BACKEND = process.env.REACT_APP_API_URL_BACKEND;
-export const API_URL_BACKEND = "https://backend-bank-app-production.up.railway.app:3000";
-const AXIOS_TIMEOUT_MS = process.env.REACT_APP_AXIOS_TIMEOUT_MS || 30000;
+export const API_URL_BACKEND = "https://backend-bank-app-production.up.railway.app:8080";
+const AXIOS_TIMEOUT_MS = process.env.REACT_APP_AXIOS_TIMEOUT_MS || 10000;
 
 const defaultHeaders = {
   Accept: 'application/json',
